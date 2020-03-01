aa32740c921e16690e6f168efcc28673a0c9272c
AmyWeitzman <amyweitzman@yahoo.com>
2020-03-01 12:36:28 -0800

    added optional features

--- app/src/main/java/com/example/footballfan/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/footballfan/flashcardapp/AddCardActivity.java
@@ -3,17 +3,50 @@ package com.example.footballfan.flashcardapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
+    private boolean editing = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        if(getIntent().getExtras().containsKey("question")) {
+            // editing card so fill in current question and answer choices
+            String question = getIntent().getStringExtra("question");
+            TextView q = (TextView) findViewById(R.id.questionInput);
+            q.setText(question);
+
+            String correctAnswer = getIntent().getStringExtra("correctAnswer");
+            TextView a1 = (TextView) findViewById(R.id.answerInputCorrect);
+            a1.setText(correctAnswer);
+
+            String wrongAnswer1 = getIntent().getStringExtra("wrongAnswer1");
+            TextView a2 = (TextView) findViewById(R.id.answerInputWrong1);
+            a2.setText(wrongAnswer1);
+
+            String wrongAnswer2 = getIntent().getStringExtra("wrongAnswer2");
+            TextView a3 = (TextView) findViewById(R.id.answerInputWrong2);
+            a3.setText(wrongAnswer2);
+        }
+
+        String is_editing = getIntent().getStringExtra("is_editing");
+        if(is_editing.equals("true"))
+            editing = true;
+        else  // "false"
+            editing = false;
+
         findViewById(R.id.removeIcon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("cancelled", "true");
+                setResult(RESULT_OK, intent);
                 finish();
             }
         });
@@ -21,14 +54,37 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.saveIcon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();//AddCardActivity.this, MainActivity.class);
                 String question = ((EditText) findViewById(R.id.questionInput)).getText().toString();
-                String answer = ((EditText) findViewById(R.id.answerInput)).getText().toString();
-                intent.putExtra("question", question);
-                intent.putExtra("answer", answer);
-                setResult(RESULT_OK, intent);
-                finish();
+                String correctAnswer = ((EditText) findViewById(R.id.answerInputCorrect)).getText().toString();
+                String wrongAnswer1 = ((EditText) findViewById(R.id.answerInputWrong1)).getText().toString();
+                String wrongAnswer2 = ((EditText) findViewById(R.id.answerInputWrong2)).getText().toString();
+                if(question.equals("") || correctAnswer.equals("") || wrongAnswer1.equals("") || wrongAnswer2.equals("")){
+                    // show error message in toast if either question or answer is empty
+                   Toast errMsg = Toast.makeText(getApplicationContext(), "Must enter question and all answer choices", Toast.LENGTH_SHORT);
+                   errMsg.setGravity(Gravity.CENTER_VERTICAL, 0, 600);
+                   errMsg.show();
+                }
+                else {
+                    Intent intent;
+                    if (getEditing()) {
+                        intent = new Intent(AddCardActivity.this, MainActivity.class);
+
+                    } else {
+                        intent = new Intent();
+                    }
+                    intent.putExtra("question", question);
+                    intent.putExtra("correctAnswer", correctAnswer);
+                    intent.putExtra("wrongAnswer1", wrongAnswer1);
+                    intent.putExtra("wrongAnswer2", wrongAnswer2);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
             }
         });
+    }
+
+    private boolean getEditing()
+    {
+        return editing;
     }
 }
--- app/src/main/java/com/example/footballfan/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/footballfan/flashcardapp/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.footballfan.flashcardapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
                     ((ImageView) findViewById(R.id.toggleVisibility)).setImageResource(R.drawable.invisible_icon);
                     showingAnswers = true;
                 }
-
             }
         });
 
@@ -82,22 +86,61 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("is_editing", "false");
                 MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+        findViewById(R.id.editIcon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                String curQ = ((TextView) findViewById(R.id.questionText)).getText().toString();
+                String a1 = ((TextView) findViewById(R.id.answerCorrect)).getText().toString();
+                String a2 = ((TextView) findViewById(R.id.answerChoice1)).getText().toString();
+                String a3 = ((TextView) findViewById(R.id.answerChoice2)).getText().toString();
+                intent.putExtra("question", curQ);
+                intent.putExtra("correctAnswer", a1);
+                intent.putExtra("wrongAnswer1", a2);
+                intent.putExtra("wrongAnswer2", a3);
+                intent.putExtra("is_editing", "true");
+                MainActivity.this.startActivityForResult(intent, 200);
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 100)
+        if((requestCode == 100) || (requestCode == 200))
         {
-            String question = data.getExtras().getString("question");
-            TextView q = (TextView) findViewById(R.id.questionText);
-            q.setText(question);
+            if(!data.getExtras().containsKey("cancelled"))
+            {
+                String question = data.getExtras().getString("question");
+                TextView q = (TextView) findViewById(R.id.questionText);
+                q.setText(question);
 
-            String answer = data.getExtras().getString("answer");
-            TextView a = (TextView) findViewById(R.id.answerText);
-            a.setText(answer);
+                String correctAnswer = data.getExtras().getString("correctAnswer");
+                String wrongAnswer1 = data.getExtras().getString("wrongAnswer1");
+                String wrongAnswer2 = data.getExtras().getString("wrongAnswer2");
+                TextView answer = (TextView) findViewById(R.id.answerText);
+                answer.setText(correctAnswer);
+
+                // place answer choices randomly
+                TextView a1 = (TextView) findViewById(R.id.answerCorrect);
+                a1.setText(correctAnswer);
+                a1.setBackgroundColor(getResources().getColor(R.color.light_blue));
+                TextView a2 = (TextView) findViewById(R.id.answerChoice1);
+                a2.setText(wrongAnswer1);
+                a2.setBackgroundColor(getResources().getColor(R.color.light_blue));
+                TextView a3 = (TextView) findViewById(R.id.answerChoice2);
+                a3.setText(wrongAnswer2);
+                a3.setBackgroundColor(getResources().getColor(R.color.light_blue));
+
+                ArrayList<TextView> answerChoices = new ArrayList<>(Arrays.asList(a1, a2, a3));
+                Collections.shuffle(answerChoices);
+
+                Snackbar.make(findViewById(R.id.questionText), "Card successfully created", Snackbar.LENGTH_SHORT).show();
+            }
         }
     }
 }
